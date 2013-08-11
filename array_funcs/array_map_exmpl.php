e43495697d995e3f42349f69dff0abc666cd7999
admix <admix.snurnikov@gmail.com>
2013-08-11 19:47:22 -0400

    Expanded the example with array_map()

--- array_funcs/array_map_exmpl.php
+++ array_funcs/array_map_exmpl.php
@@ -3,8 +3,19 @@
 	{
 		return ($num * $num * $num * $num);
 	}
-
+	
 	$a = array(1,2,3,4,5,6,7);
 	$b = array_map("fourth", $a);
 	print_r($b);
+
+/* ---------------------------------------- */
+
+	function concat($str) //making all letters in the string UPPERCASE
+	{
+		return strtoupper($str);
+	}
+	
+	$str = array("Bob","Matt","Tom","Alex","Michael");
+	$out = array_map("concat", $str);
+	print_r($out);
 ?>
